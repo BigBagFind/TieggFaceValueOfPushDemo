be89ec7e208db7f971b572e9852d0ec4d7e0177e
BigBagFind <594228924@qq.com>
2015-11-12 22:38:36 +0800

    Initial Commit

---
+++ TieggFaceValueOfPushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TieggFaceValueOfPushDemo
+//
+//  Created by 吴玉铁 on 15/11/12.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
